e2dd1dd84b3520ede23ddd867083480d0cd93459
Rullyan <rullyan.gs@hotmail.com>
2021-11-29 22:56:01 -0300

    add CORS

--- server.js
+++ server.js
@@ -2,13 +2,11 @@ const express = require('express');
 const serveStatic = require("serve-static")
 const path = require('path');
 app = express();
+
+const cors = require("cors") // importing the `cors` package
+app.use(cors()) // tells Express to use `cors`, and solves the issue
+
 app.use(serveStatic(path.join(__dirname, 'dist')));
 const port = process.env.PORT || 3000;
 app.listen(port);
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
-  
