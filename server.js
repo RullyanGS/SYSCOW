478c43b5889418a165b54f22e3dbc09431d9853d
Rullyan <rullyan.gs@hotmail.com>
2021-11-29 22:42:42 -0300

    correção CORS

--- server.js
+++ server.js
@@ -5,3 +5,10 @@ app = express();
 app.use(serveStatic(path.join(__dirname, 'dist')));
 const port = process.env.PORT || 3000;
 app.listen(port);
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+  
